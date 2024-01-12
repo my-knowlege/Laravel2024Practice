3ec73d08cf525b834f10a68f46467dfededaedc8
gagan <gagandeepk.offsureit@gmail.com>
2024-01-12 11:37:32 +0530

    fixes

--- resources/views/featured.blade.php
+++ resources/views/featured.blade.php
@@ -2,8 +2,6 @@
 <p>New changes</p>
 <h1>New fixes</h1>
 <h1>hello<h1>
-<<<<<<< HEAD
 <h1>after completing all crs we are working new module functionality</h1>
-=======
 <h1>Tommorow we are working on some new crs</h1>
->>>>>>> 2661db31871326084ec6cb5e5e3ced42ecbfa6ec
+<h1>Hii</h1>
